70ebf24a174f5d845e82f661960ec2649ac8e10e
Peter Suhm <peter@suhm.dk>
2014-03-09 15:15:03 +0100

    fix hardcoded extension

--- src/Thin/PostLoader.php
+++ src/Thin/PostLoader.php
@@ -27,7 +27,7 @@ class PostLoader implements DocumentLoaderInterface {
             $post = new Post;
             $post->metadata = json_decode($content[0], true);
             $post->content = $content[1];
-            $post->slug = basename($file, '.md');
+            $post->slug = basename($file, $this->settings['document_ext']);
 
             $documentCollection->add($post);
         }
